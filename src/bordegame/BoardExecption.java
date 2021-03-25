6730880bcf6d784b567debf73dea53f79170d2f6
Ronaldo Santos <jronaldojr@gmail.com>
2021-03-25 19:15:43 -0300

    BoardExecption and defensive programming

---
+++ src/bordegame/BoardExecption.java
@@ -0,0 +1,10 @@
+package bordegame;
+
+public class BoardExecption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+		
+	public BoardExecption(String msg) {
+		super(msg);
+	}
+
+}
